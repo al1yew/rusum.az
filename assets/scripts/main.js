b8229ff3f3e62ccbe3759cd43f5b4021bc6e08b5
al1yew <vasifja@code.edu.az>
2022-11-28 20:32:12 +0400

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,30 @@
 
 $(document).ready(function () {
 
+    $(document).on('submit', '#mainForm', function (e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
 
+        let autoType = formData.get('autoType');
+        let engineType = formData.get('engineType');
+        let volume = formData.get('volume');
+        let price = formData.get('price');
 
+        console.log(autoType);
+        console.log(engineType);
+        console.log(volume);
+        console.log(price);
+
+        // formData.forEach(element => {
+        //     console.log(element)
+        // });
+
+        //dalshe budem proverat uje
+    });
+
+    $(document).on('click', '.inplabelkeeper', function () {
+
+        $(this).siblings('label').removeClass('isselected')
+        $(this).addClass('isselected');
+    });
 });
